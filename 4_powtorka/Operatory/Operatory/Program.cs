47cb8ad7eb328ae5dcc536b9f4070d7cd47cc1c0
ArekModrzejewski <arekmod98@gmail.com>
2023-03-09 22:49:02 +0100

    finish Operatory excercises

--- 4_powtorka/Operatory/Operatory/Program.cs
+++ 4_powtorka/Operatory/Operatory/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            //Zadanie1
+            zadanie1();
+            zadanie2();
+            zadanie3();
+            zadanie4();
+            zadanie5();
+        }
+        static void zadanie1()
+        {
             int number = 7;
             float money = 2.5f;
             string text = "Default text";
@@ -17,15 +24,18 @@
             Console.WriteLine($"isLogged = {isLogged}");
             Console.WriteLine($"myChar = {myChar}");
             Console.WriteLine($"price = {price}");
+        }
 
-            //Zadanie2
+        static void zadanie2()
+        {
             string myAge = "Age: ";
             int wifeAge = 18;
             string result = myAge + wifeAge;
             Console.WriteLine(result);
             //Wniosek: C# automatycznie "castuje" int na string
+        }
 
-            //Zadanie3
+        static void zadanie3() {
             bool isTrue = true;
             bool isFalse = false;
             bool isReallyTrue = true;
@@ -35,6 +45,27 @@
             Console.WriteLine($"and = {and}");
             Console.WriteLine($"or = {or}");
             Console.WriteLine($"negative = {negative}");
+        }
+        static void zadanie4() {
+            int a = 5;
+            int b = 12;
+            int add = a + b;
+            int sub = a - b;
+            double div = a / b;
+            int mul = a * b;
+            int mod = a % b;
+            Console.WriteLine($"add = {add}");
+            Console.WriteLine($"sub = {sub}");
+            Console.WriteLine($"div = {div}");
+            Console.WriteLine($"mul = {mul}");
+            Console.WriteLine($"mod = {mod}");
+        }
+        static void zadanie5() {
+            string a = "Ala ";
+            string b = "ma ";
+            string c = "kota.";
+            string result = a + b + c;
+            Console.WriteLine($"result = {result}");
         }
     }
 }
